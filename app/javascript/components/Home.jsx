10541f9733a3110ad733a02048006268d526c0bd
wstenzel <stenzel.will@gmail.com>
2020-05-22 13:56:14 -0700

    Create buttons for multiple entry types

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,6 +5,9 @@ import { Button } from 'antd';
 export default () => (
     <>
         <h1>Journal App</h1>
-        <Button type="primary"><Link to="/entries/new">Create a new entry</Link></Button>
+        <Button type="primary"><Link to="/entries/new?type=daily">Create a new daily reflection entry</Link></Button>
+        <br/>
+        <br/>
+        <Button type="primary"><Link to="/entries/freeform?type=freeform">Create a new free form entry</Link></Button>
     </>
 );

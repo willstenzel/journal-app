733358cae696fd8573302374cd3b29a51baee965
Arun Jeevanantham <itsmearun98@gmail.com>
2020-05-25 16:16:13 -0400

    add typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
-
+  environment.loaders.append('typescript', {
+    test: /.(ts|tsx)$/,
+    loader: 'ts-loader'
+  });
 module.exports = environment

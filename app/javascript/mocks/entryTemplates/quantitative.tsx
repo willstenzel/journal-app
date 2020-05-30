0519e7607f60edefe545d5b9b29ed42005ffd37a
Arun Jeevanantham <itsmearun98@gmail.com>
2020-05-30 14:05:22 -0400

    add an example quantative

--- app/javascript/mocks/entryTemplates/quantitative.tsx
+++ app/javascript/mocks/entryTemplates/quantitative.tsx
@@ -1,13 +1,17 @@
 import { EntryTemplate } from "../../models/types";
 
+let q1 = require("../blockTemplates/quantitative_1");
+let q2 = require("../blockTemplates/quantitative_2");
+let q3 = require("../blockTemplates/quantitative_3");
+
 export function quickDaily(): EntryTemplate {
     let entry = {
         title: "quick daily",
         id: 1,
-        blocks: [],
+        blocks: [q1, q2, q3],
         emojis: [],
         canUploadPhotos: false
     }
-
+    
     return entry;
 }

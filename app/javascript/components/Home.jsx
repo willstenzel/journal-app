54041860a6124f0bf6cfe490c24920abe2a826f3
wstenzel <stenzel.will@gmail.com>
2020-06-03 00:32:37 -0700

    Dynamically render buttons from templateList

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from 'antd';
+import { getTemplateList } from '../mocks/templatesList'
+import { TemplateInfo } from "../models/types";
 
-export default () => (
-    <>
-        <h1>Journal App</h1>
-        <Button type="primary"><Link to="/entries/new?type=daily">Create a new daily reflection entry</Link></Button>
-        <br/>
-        <br/>
-        <Button type="primary"><Link to="/entries/new?type=freeform">Create a new free form entry</Link></Button>
-    </>
-);
+export default () => {
+    const templatesList = getTemplateList();
+    return (
+        <>
+            <h1>Journal App</h1>
+            {templatesList.map((template) => (
+                <>
+                    <Button key={template.id} type="primary"><Link to={`/entries/new?type=${template.key}`}>Create a new {template.name} entry</Link></Button>
+                    <br />
+                    <br />
+                </>
+            ))}
+        </>
+    )
+};

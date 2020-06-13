a1eab2d6438e73fdb5419facf5b7fc0d459f1294
Arun Jeevanantham <itsmearun98@gmail.com>
2020-06-13 17:18:41 -0400

    function to class component

--- app/javascript/components/NewEntry.tsx
+++ app/javascript/components/NewEntry.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Divider } from "antd";
 import { useLocation } from "react-router-dom";
-import { EntryTemplate, BlockTemplate } from "../models/types";
+import { EntryTemplate, BlockTemplate, Entry, Block } from "../models/types";
 import { mockQuantitative } from "../mocks/entryTemplates/quantitative";
 import { mockFreeForm } from "../mocks/entryTemplates/freeForm";
 import { mockQualitative } from "../mocks/entryTemplates/qualitative";
@@ -10,23 +10,6 @@ import { Header } from "../components/blocks/Header"
 import { ShortResponse } from "../components/blocks/ShortResponse"
 import { LongResponse } from "../components/blocks/LongResponse"
 
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
-
-function getTemplate(type): EntryTemplate {
-  switch (type) {
-    case 'freeform':
-      return mockFreeForm();
-    case 'qualitative':
-      return mockQualitative();
-    case 'quantitative':
-      return mockQuantitative();
-    default:
-      return mockFreeForm();
-  }
-}
-
 const Components = {
   Quantitative: Quantitative,
   Header: Header,
@@ -34,36 +17,75 @@ const Components = {
   LongResponse: LongResponse,
 }
 
-function BlockToComponent(block: BlockTemplate) {
-  // component does exist
-  if (typeof Components[block.type] !== "undefined") {
-    return React.createElement(Components[block.type], {
-      key: block.id,
-      ...block
-    });
+export class NewEntry extends React.Component {
+  state: { response: Entry; template: EntryTemplate; };
+
+  constructor(props) {
+    super(props);
+    const templateType = new URLSearchParams(window.location.search).get("type");
+    const template = this.getTemplate(templateType);
+
+    const entry: Entry = {
+      title: template.title,
+      templateId: template.id,
+      blocks: [],
+      emojiSelected: [],
+      photoLinks: []
+    }
+
+    this.state = {
+      response: entry,
+      template: template
+    }
   }
-  // component doesn't exist yet
-  return React.createElement(
-    () => <div>The component {block.type} has not been created yet.</div>,
-    { key: block.id }
-  );
+
+  useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
+  
+  getTemplate(type): EntryTemplate {
+    switch (type) {
+      case 'freeform':
+        return mockFreeForm();
+      case 'qualitative':
+        return mockQualitative();
+      case 'quantitative':
+        return mockQuantitative();
+      default:
+        return mockFreeForm();
+    }
+  }
+  
+  blockToComponent(block: BlockTemplate) {
+    // component does exist
+    if (typeof Components[block.type] !== "undefined") {
+      return React.createElement(Components[block.type], {
+        key: block.id,
+        ...block
+      });
+    }
+    // component doesn't exist yet
+    return React.createElement(
+      () => <div>The component {block.type} has not been created yet.</div>,
+      { key: block.id }
+    );
+  }
+
+  handleOnChange() {
+    
+  }
+
+  render() {
+    const blockComponents = this.state.template.blocks.map(block => this.blockToComponent(block));
+
+    return (
+      <div>
+        <h4><b>{this.state.template.title}</b></h4>
+        <Divider />
+        {blockComponents}
+      </div>
+    );
+  }
 }
 
-export const NewEntry: React.FC = (props: any) => {
-  // TODO: set query paramater and fetch right template
-  const query = useQuery();
-  console.log(query.get('type'));
 
-  const [template, setTemplate] = useState(getTemplate(query.get('type')));
-  const [responseArray, setResponseArray] = useState([]);
-
-  const blocks: BlockTemplate[] = template.blocks;
-
-  return (
-    <div>
-      <h4><b>{template.title}</b></h4>
-      <Divider />
-      {blocks.map(block => BlockToComponent(block))}
-    </div>
-  );
-};

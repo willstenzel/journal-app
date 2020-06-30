aadbacd65e39c759b8f2375a429f7d2c042467fd
Arun Jeevanantham <itsmearun98@gmail.com>
2020-06-30 19:19:37 -0400

    add slider

--- app/javascript/components/blocks/Quantitative.tsx
+++ app/javascript/components/blocks/Quantitative.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import { BlockTemplate, NumberRange }  from "../../models/types"
+import { Slider, Form } from "antd";
 
-export const Quantitative: React.FC = (props: any) => {
+export const Quantitative: React.FC = (props: BlockTemplate) => {
+    const range: NumberRange = props.range ? props.range : {min: 1, max: 10};
+    const defaultValue = (range.min + range.max)/2;
     return (
         <div>
             <h1>{props.title}</h1>
-            <p>{props.range.min}</p>
-            <p>{props.range.max}</p>
+            <Form.Item
+                name={props.id}
+                children={
+                    <Slider 
+                        defaultValue={defaultValue} 
+                        min={range.min} 
+                        max={range.max}/>}
+            />
         </div>
     );
 }

3b1ecdf259678015352b424be0506c136ee2aa70
willstenzel <stenzel.will@gmail.com>
2020-07-12 12:56:06 -0600

    Add models and create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+free_form_template = EntryTemplate.create!({ title: "Free Form"});
+free_form_question = Question.create!({ 
+    title: "Body", 
+    entry_template_id: free_form_template.id,
+    question_type: 1  # This should map to the BlockType.LongResponse type on the frontend
+})
+
+Entry.create!({
+    title: "My First Entry",
+    contents: "https://some_s3_bucket.com",
+    emojis: "U+1F92F",
+})

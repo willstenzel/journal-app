bd62f74f5d8ca817d9389e9bd4d5d420997d2ef3
gamburgm <mitch.gamburg@gmail.com>
2020-07-19 18:30:22 -0400

    login, timing out

--- app/javascript/components/auth/Login.tsx
+++ app/javascript/components/auth/Login.tsx
@@ -1,28 +1,40 @@
 import React, { useState } from 'react';
-import Form from 'antd/lib/form/Form';
-// import Button from 'antd/lib/button/Button';
-import { Input, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import Form  from 'antd/lib/form/Form';
+import { Input, Button, Form as FormType } from 'antd';
 import styled from 'styled-components';
 
+const Container = styled.div`
+    width: 100%;
+`;
+
+const ContentContainer = styled.div`
+  width: 50%;
+  margin: auto;
+`;
 
 export const Login: React.FC = (props: any) => {
   return (
-      <Form name='Login Form'>
-        <Form.Item
-          label='Username'
-          name='username'
-          rules={[{ required: true }]}
-        >
-          <Input/>
-        </Form.Item>
-        <Form.Item
-          label='Password'
-          name='password'
-          rules={[{ required: true }]}
-        >
-          <Input/>
-        </Form.Item>
-        <input type='submit' value='Submit'/>
-      </Form>
+    <Container>
+      <ContentContainer>
+        <Form name='Login Form'>
+          <FormType.Item
+            label='Username'
+            name='username'
+            rules={[{ required: true }]}
+            children={
+              <Input prefix={<UserOutlined/>} placeholder='Username'/>
+            }
+          />
+          <FormType.Item
+            label='Password'
+            name='password'
+            rules={[{ required: true }]}
+            children={<Input/>}
+          />
+          <input type='submit' value='Submit'/>
+        </Form>
+      </ContentContainer>
+    </Container>
   );
 }
